04dad08e28644f43126d638c89413f3b9cab23b5
igorbbd <igorbbd30@gmail.com>
2019-03-24 21:33:33 +0300

    feat: implement checkBrackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBracket, closeBracket;
+    let checkBrackets = str.split("");
+    for(let i = 0; i < str.length; i++){
+      for(let j = 0; j < bracketsConfig.length; j++){
+        openBracket = bracketsConfig[j][0] 
+        closeBracket = bracketsConfig[j][1]              
+          if(checkBrackets[i] == openBracket && checkBrackets[i+1] == closeBracket){
+            checkBrackets.splice(i,2)
+            i = -1;  
+          }else{                   
+           continue;
+          };               
+        };  
+    };  
+    if(checkBrackets.length == 0){
+      return true;
+    }else{
+      return false;
+    };  
+};
